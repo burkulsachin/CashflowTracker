3fed021f52219f2ecc3a1fa28b6f118e4a5f9790
sachinburkul2006 <sachinburkul2006@gmail.com>
2026-03-11 07:24:32 +0000

    n ew

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+  apiKey: "AIzaSyDAA5Ga89HFW3bOiq7NkJnKHfeHsDBvfRQ",
+  authDomain: "cashflow-2169e.firebaseapp.com",
+  projectId: "cashflow-2169e",
+  storageBucket: "cashflow-2169e.appspot.com",
+  messagingSenderId: "235944831965",
+  appId: "1:235944831965:web:de3c8554090e57666fe434",
+  measurementId: "G-NJKRRPE9QM"
 };
